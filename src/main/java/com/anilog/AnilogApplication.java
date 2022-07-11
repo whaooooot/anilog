e649df15c068731cda4411b2acc2e000423d1e23
Ahn HeeChang <gmlckd6219@gmail.com>
2022-07-12 02:57:44 +0900

    project commit

---
+++ src/main/java/com/anilog/AnilogApplication.java
@@ -0,0 +1,13 @@
+package com.anilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnilogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnilogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anilog/AnilogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnilogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
