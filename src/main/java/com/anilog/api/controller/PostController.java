b036df38ea010d9e7a307c6eb50711a1a1961c5f
Ahn HeeChang <gmlckd6219@gmail.com>
2022-07-13 03:02:46 +0900

    -ani post 데이터 콘텐츠 타입

--- src/main/java/com/anilog/api/controller/PostController.java
+++ src/main/java/com/anilog/api/controller/PostController.java
@@ -1,13 +1,44 @@
 package com.anilog.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.anilog.api.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+
+@Slf4j
 @RestController
 public class PostController {
 
-    @GetMapping("/posts")
-    public String get() {
+    //Http Method
+    //GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
+    // 글 등록
+    //POST Method
+
+    @PostMapping("/posts")
+    public String post(@RequestBody PostCreate params) {
+
+        /*
+         * 테스트에서 param으로 넘김
+         */
+
+        //@RequestParam String title, @RequestParam String content
+        //log.info("title={}, content={}", title, content);
+
+        //@RequestParam Map<String, String> params
+        //log.info("params={}", params);
+        //String title = params.get("title");
+
+        //PostCreate params //PostCreate 만들고 사용
+        //log.info("params={}", params.toString());
+
+        /*
+         * 테스트에서 json값으로 넘김
+         */
+        //@RequestBody PostCreate params
+        log.info("params={}", params.toString());
+
         return "Hello world";
     }
 }
---
+++ src/main/java/com/anilog/api/request/PostCreate.java
@@ -0,0 +1,25 @@
+package com.anilog.api.request;
+
+
+public class PostCreate {
+
+    public String title;
+    public String content;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    //lombok에 있는 class위에 @ToStirng() 붙여서 사용해도됨
+    @Override
+    public String toString() {
+        return "PostCreate{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
